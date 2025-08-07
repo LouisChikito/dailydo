ac8582d4dd2021509d0113fb9985664ff9386bd6
Jairo Daniel Raya Morales <jdaniel.2622@gmail.com>
2025-08-07 10:36:37 -0600

    feat: Add Supabase auth callback route handler

---
+++ src/app/auth/callback/route.ts
@@ -0,0 +1,22 @@
+// src/app/auth/callback/route.ts
+
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
+
+import type { NextRequest } from 'next/server'
+
+export const dynamic = 'force-dynamic'
+
+export async function GET(request: NextRequest) {
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get('code')
+
+  if (code) {
+    const supabase = createRouteHandlerClient({ cookies })
+    await supabase.auth.exchangeCodeForSession(code)
+  }
+
+  // URL a la que redirigir después de que el usuario inicie sesión
+  return NextResponse.redirect(requestUrl.origin)
+}
